4d39f0277ddff1e7da24e90e5fb6f97d6cf2c793
Enrico Zerilli <ezerilli@gatech.edu>
2019-11-21 16:28:46 +0100

    Add comments and docstring

--- Unsupervised_Learning/run_experiments.py
+++ Unsupervised_Learning/run_experiments.py
@@ -1,10 +1,6 @@
 # Script to run experiments
 
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import utils
 
 from clustering import KMeansClustering, MixtureOfGaussians
 from dimensionality_reduction import IndependentComponents, KernelPrincipalComponents, \
@@ -16,7 +12,7 @@ from sklearn.datasets import load_breast_cancer, fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-IMAGE_DIR = 'images/'
+IMAGE_DIR = 'images/'  # output images directory
 
 
 def load_dataset(dataset='WDBC', split_percentage=0.2):
@@ -25,7 +21,6 @@ def load_dataset(dataset='WDBC', split_percentage=0.2):
        Args:
            dataset (string): dataset, WDBC or MNIST.
            split_percentage (float): validation split.
-           visualize (bool): True if some of the dataset images have to been shown.
 
        Returns:
            x_train (ndarray): training data.
@@ -78,27 +73,38 @@ def load_dataset(dataset='WDBC', split_percentage=0.2):
 
 
 def clustering(x_train, x_test, y_train, **kwargs):
-    """Perform experiment.
+    """Perform clustering experiment.
 
         Args:
            x_train (ndarray): training data.
+           x_test (ndarray): test data.
            y_train (ndarray): training labels.
+           kwargs (dict): additional arguments to pass:
+                    - dataset (string): dataset, WDBC or MNIST.
+                    - perform_model_complexity (bool): True if we want to perform model complexity.
+                    - kmeans_n_clusters (int): number of clusters for k-Means.
+                    - gmm_n_clusters (int): number of clusters for Gaussians Mixture Models.
+                    - gmm_covariance (string): covariance type for Gaussians Mixture Models.
 
         Returns:
-           None.
+           kmeans_clusters (ndarray): k-Means cluster on training and test data.
+           gmm_clusters (ndarray): Gaussian Mixture Models cluster on training and test data.
         """
 
     print('\n--------------------------')
     print('kMeans')
 
+    # Declare kMeans, perform experiments and get clusters on training data
     kmeans = KMeansClustering(n_clusters=kwargs['kmeans_n_clusters'], max_n_clusters=10)
     kmeans_clusters = kmeans.experiment(x_train, x_test, y_train,
                                         dataset=kwargs['dataset'],
                                         perform_model_complexity=kwargs['perform_model_complexity'])
 
     print('\n--------------------------')
-    print('EM')
-    gmm = MixtureOfGaussians(n_clusters=kwargs['em_n_clusters'], covariance=kwargs['em_covariance'], max_n_clusters=10)
+    print('GMM')
+
+    # Declare Gaussian Mixtures Models, perform experiments and get clusters on training data
+    gmm = MixtureOfGaussians(n_clusters=kwargs['gmm_n_clusters'], covariance=kwargs['gmm_covariance'], max_n_clusters=10)
     gmm_clusters = gmm.experiment(x_train, x_test, y_train,
                                   dataset=kwargs['dataset'],
                                   perform_model_complexity=kwargs['perform_model_complexity'])
@@ -107,21 +113,44 @@ def clustering(x_train, x_test, y_train, **kwargs):
 
 
 def dimensionality_reduction(x_train, x_test, y_train, **kwargs):
-    """Perform experiment.
+    """Perform dimensionality reduction experiment.
 
         Args:
            x_train (ndarray): training data.
            x_test (ndarray): test data.
            y_train (ndarray): training labels.
+           kwargs (dict): additional arguments to pass:
+                    - dataset (string): dataset, WDBC or MNIST.
+                    - perform_model_complexity (bool): True if we want to perform model complexity.
+                    - pca_n_components (int): number of components for PCA.
+                    - pca_kmeans_n_clusters (int): number of clusters for PCA + k-Means.
+                    - pca_gmm_n_clusters (int): number of clusters for PCA + Gaussians Mixtures Models.
+                    - pca_gmm_covariance (string): covariance type for PCA + Gaussians Mixture Models.
+                    - ica_n_components (int): number of components for ICA.
+                    - ica_kmeans_n_clusters (int): number of clusters for ICA + k-Means.
+                    - ica_gmm_n_clusters (int): number of clusters for ICA + Gaussians Mixtures Models.
+                    - ica_gmm_covariance (string): covariance type for ICA + Gaussians Mixture Models.
+                    - kpca_n_components (int): number of components for KPCA.
+                    - kpca_kmeans_n_clusters (int): number of clusters for KPCA + k-Means.
+                    - kpca_gmm_n_clusters (int): number of clusters for KPCA + Gaussians Mixtures Models.
+                    - kpca_gmm_covariance (string): covariance type for KPCA + Gaussians Mixture Models.
+                    - rp_n_components (int): number of components for RP.
+                    - rp_kmeans_n_clusters (int): number of clusters for RP + k-Means.
+                    - rp_gmm_n_clusters (int): number of clusters for RP + Gaussians Mixtures Models.
+                    - rp_gmm_covariance (string): covariance type for RP + Gaussians Mixture Models.
 
         Returns:
-           None.
+           x_pca (ndarray): reduced dataset by PCA.
+           x_ica (ndarray): reduced dataset by ICA.
+           x_kpca (ndarray): reduced dataset by KPCA.
+           x_rp (ndarray): reduced dataset by RP.
         """
 
     print('\n--------------------------')
     print('PCA')
     print('--------------------------')
 
+    # Declare PCA, perform experiments by reducing the dataset and perform clustering experiments on it
     pca = PrincipalComponents(n_components=kwargs['pca_n_components'])
     x_pca = pca.experiment(x_train, x_test, y_train,
                            dataset=kwargs['dataset'],
@@ -130,13 +159,14 @@ def dimensionality_reduction(x_train, x_test, y_train, **kwargs):
     clustering(x_pca[0],  x_pca[1], y_train,
                dataset=kwargs['dataset'] + '_pca_reduced',
                kmeans_n_clusters=kwargs['pca_kmeans_n_clusters'],
-               em_n_clusters=kwargs['pca_em_n_clusters'], em_covariance=kwargs['pca_em_covariance'],
+               em_n_clusters=kwargs['pca_gmm_n_clusters'], em_covariance=kwargs['pca_gmm_covariance'],
                perform_model_complexity=kwargs['perform_model_complexity'])
 
     print('\n--------------------------')
     print('ICA')
     print('--------------------------')
 
+    # Declare ICA, perform experiments by reducing the dataset and perform clustering experiments on it
     ica = IndependentComponents(n_components=kwargs['ica_n_components'])
     x_ica = ica.experiment(x_train, x_test, y_train,
                            dataset=kwargs['dataset'],
@@ -145,13 +175,14 @@ def dimensionality_reduction(x_train, x_test, y_train, **kwargs):
     clustering(x_ica[0],  x_ica[1], y_train,
                dataset=kwargs['dataset'] + '_ica_reduced',
                kmeans_n_clusters=kwargs['ica_kmeans_n_clusters'],
-               em_n_clusters=kwargs['ica_em_n_clusters'], em_covariance=kwargs['ica_em_covariance'],
+               em_n_clusters=kwargs['ica_gmm_n_clusters'], em_covariance=kwargs['ica_gmm_covariance'],
                perform_model_complexity=kwargs['perform_model_complexity'])
 
     print('\n--------------------------')
     print('KPCA')
     print('--------------------------')
 
+    # Declare KPCA, perform experiments by reducing the dataset and perform clustering experiments on it
     kpca = KernelPrincipalComponents(n_components=kwargs['kpca_n_components'], kernel=kwargs['kpca_kernel'])
     x_kpca = kpca.experiment(x_train, x_test, y_train,
                              dataset=kwargs['dataset'],
@@ -160,13 +191,14 @@ def dimensionality_reduction(x_train, x_test, y_train, **kwargs):
     clustering(x_kpca[0], x_kpca[1], y_train,
                dataset=kwargs['dataset'] + '_kpca_reduced',
                kmeans_n_clusters=kwargs['kpca_kmeans_n_clusters'],
-               em_n_clusters=kwargs['kpca_em_n_clusters'], em_covariance=kwargs['kpca_em_covariance'],
+               em_n_clusters=kwargs['kpca_gmm_n_clusters'], em_covariance=kwargs['kpca_gmm_covariance'],
                perform_model_complexity=kwargs['perform_model_complexity'])
 
     print('\n--------------------------')
     print('RP')
     print('--------------------------')
 
+    # Declare RP, perform experiments by reducing the dataset and perform clustering experiments on it
     rp = RandomProjections(n_components=kwargs['rp_n_components'])
     x_rp = rp.experiment(x_train, x_test, y_train,
                          dataset=kwargs['dataset'],
@@ -175,18 +207,43 @@ def dimensionality_reduction(x_train, x_test, y_train, **kwargs):
     clustering(x_rp[0], x_rp[1], y_train,
                dataset=kwargs['dataset'] + '_rp_reduced',
                kmeans_n_clusters=kwargs['rp_kmeans_n_clusters'],
-               em_n_clusters=kwargs['rp_em_n_clusters'], em_covariance=kwargs['rp_em_covariance'],
+               em_n_clusters=kwargs['rp_gmm_n_clusters'], em_covariance=kwargs['rp_gmm_covariance'],
                perform_model_complexity=kwargs['perform_model_complexity'])
 
     return x_pca, x_ica, x_kpca, x_rp
 
 
-def neural_network(x_train, x_test, y_train, y_test, x_pca, x_ica, x_kpca, x_rp, x_kmeans, x_gmm, **kwargs):
+def neural_network(x_train, x_test, y_train, y_test,
+                   x_pca, x_ica, x_kpca, x_rp,
+                   x_kmeans, x_gmm, **kwargs):
+
+    """Perform neural network experiment.
+
+            Args:
+               x_train (ndarray): training data.
+               x_test (ndarray): test data.
+               y_train (ndarray): training labels.
+               y_test (ndarray): test labels.
+               x_pca (ndarray): reduced dataset by PCA.
+               x_ica (ndarray): reduced dataset by ICA.
+               x_kpca (ndarray): reduced dataset by KPCA.
+               x_rp (ndarray): reduced dataset by RP.
+               x_kmeans (ndarray): clusters produced by k-Means.
+               x_gmm (ndarray): clusters produced by Gaussian Mixture Models.
+               kwargs (dict): additional arguments to pass:
+                        - layer1_nodes (int): number of neurons in first layer.
+                        - layer2_nodes (int): number of neurons in second layer.
+                        - learning_rate (float): learning rate.
+
+            Returns:
+               None.
+            """
 
     print('\n--------------------------')
     print('NN')
     print('--------------------------')
 
+    # Declare Neural Network and perform experiments on the original dataset
     nn = NeuralNetwork(layer1_nodes=kwargs['layer1_nodes'],
                        layer2_nodes=kwargs['layer2_nodes'],
                        learning_rate=kwargs['learning_rate'])
@@ -196,6 +253,7 @@ def neural_network(x_train, x_test, y_train, y_test, x_pca, x_ica, x_kpca, x_rp,
     print('PCA + NN')
     print('--------------------------')
 
+    # Declare Neural Network and perform experiments on the reduced dataset by PCA
     nn = NeuralNetwork(layer1_nodes=kwargs['layer1_nodes'],
                        layer2_nodes=kwargs['layer2_nodes'],
                        learning_rate=kwargs['learning_rate'])
@@ -205,6 +263,7 @@ def neural_network(x_train, x_test, y_train, y_test, x_pca, x_ica, x_kpca, x_rp,
     print('ICA + NN')
     print('--------------------------')
 
+    # Declare Neural Network and perform experiments on the reduced dataset by ICA
     nn = NeuralNetwork(layer1_nodes=kwargs['layer1_nodes'],
                        layer2_nodes=kwargs['layer2_nodes'],
                        learning_rate=kwargs['learning_rate'])
@@ -214,6 +273,7 @@ def neural_network(x_train, x_test, y_train, y_test, x_pca, x_ica, x_kpca, x_rp,
     print('KPCA + NN')
     print('--------------------------')
 
+    # Declare Neural Network and perform experiments on the reduced dataset by KPCA
     nn = NeuralNetwork(layer1_nodes=kwargs['layer1_nodes'],
                        layer2_nodes=kwargs['layer2_nodes'],
                        learning_rate=kwargs['learning_rate'])
@@ -223,6 +283,7 @@ def neural_network(x_train, x_test, y_train, y_test, x_pca, x_ica, x_kpca, x_rp,
     print('RP+ NN')
     print('--------------------------')
 
+    # Declare Neural Network and perform experiments on the reduced dataset by RP
     nn = NeuralNetwork(layer1_nodes=kwargs['layer1_nodes'],
                        layer2_nodes=kwargs['layer2_nodes'],
                        learning_rate=kwargs['learning_rate'])
@@ -232,34 +293,40 @@ def neural_network(x_train, x_test, y_train, y_test, x_pca, x_ica, x_kpca, x_rp,
     print('KMEANS+ NN')
     print('--------------------------')
 
+    # Declare Neural Network
     nn = NeuralNetwork(layer1_nodes=kwargs['layer1_nodes'],
                        layer2_nodes=kwargs['layer2_nodes'],
                        learning_rate=kwargs['learning_rate'])
 
+    # Augment the original dataset by adding clusters produced by k-Means as features
     x_kmeans_normalized = (x_kmeans[0] - np.mean(x_kmeans[0])) / np.std(x_kmeans[0])
     x_kmeans_normalized = np.expand_dims(x_kmeans_normalized, axis=1)
     x_train_new = np.append(x_train, x_kmeans_normalized, axis=1)
-
     x_kmeans_normalized = (x_kmeans[1] - np.mean(x_kmeans[1])) / np.std(x_kmeans[1])
     x_kmeans_normalized = np.expand_dims(x_kmeans_normalized, axis=1)
     x_test_new = np.append(x_test, x_kmeans_normalized, axis=1)
 
+    # Perform experiments on it
     nn.experiment(x_train_new, x_test_new, y_train, y_test)
 
     print('\n--------------------------')
     print('GMM+ NN')
+    print('--------------------------')
+
+    # Declare Neural Network
     nn = NeuralNetwork(layer1_nodes=kwargs['layer1_nodes'],
                        layer2_nodes=kwargs['layer2_nodes'],
                        learning_rate=kwargs['learning_rate'])
 
+    # Augment the original dataset by adding clusters produced by Gaussian Mixture Models as features
     x_gmm_normalized = (x_gmm[0] - np.mean(x_gmm[0])) / np.std(x_gmm[0])
     x_gmm_normalized = np.expand_dims(x_gmm_normalized, axis=1)
     x_train_new = np.append(x_train, x_gmm_normalized, axis=1)
-
     x_gmm_normalized = (x_gmm[1] - np.mean(x_gmm[1])) / np.std(x_gmm[1])
     x_gmm_normalized = np.expand_dims(x_gmm_normalized, axis=1)
     x_test_new = np.append(x_test, x_gmm_normalized, axis=1)
 
+    # Perform experiments on it
     nn.experiment(x_train_new, x_test_new, y_train, y_test)
 
 
@@ -271,12 +338,14 @@ if __name__ == "__main__":
     perform_model_complexity = False
     x_train, x_test, y_train, y_test = load_dataset(dataset)
 
+    # Clustering experiments
     kmeans_clusters, gmm_clusters = clustering(x_train, x_test, y_train,
                                                dataset=dataset,
                                                kmeans_n_clusters=2,
                                                em_n_clusters=2, em_covariance='full',
                                                perform_model_complexity=perform_model_complexity)
 
+    # Dimensionality reduction experiments
     x_pca, x_ica, x_kpca, x_rp = dimensionality_reduction(x_train, x_test, y_train,
                                                           dataset=dataset,
                                                           pca_n_components=10, pca_kmeans_n_clusters=2,
@@ -290,6 +359,7 @@ if __name__ == "__main__":
                                                           rp_em_n_clusters=3, rp_em_covariance='full',
                                                           perform_model_complexity=perform_model_complexity)
 
+    # Neural Network experiments
     neural_network(x_train, x_test, y_train, y_test,
                    x_pca, x_ica, x_kpca, x_rp,
                    kmeans_clusters, gmm_clusters,
@@ -301,12 +371,14 @@ if __name__ == "__main__":
     perform_model_complexity = False
     x_train, x_test, y_train, y_test = load_dataset(dataset)
 
+    # Clustering experiments
     kmeans_clusters, gmm_clusters = clustering(x_train, x_test, y_train,
                                                dataset=dataset,
                                                kmeans_n_clusters=2,
                                                em_n_clusters=10, em_covariance='diag',
                                                perform_model_complexity=perform_model_complexity)
 
+    # Dimensionality reduction experiments
     x_pca, x_ica, x_kpca, x_rp = dimensionality_reduction(x_train, x_test, y_train,
                                                           dataset=dataset,
                                                           pca_n_components=260, pca_kmeans_n_clusters=2,
@@ -320,6 +392,7 @@ if __name__ == "__main__":
                                                           rp_em_n_clusters=2, rp_em_covariance='tied',
                                                           perform_model_complexity=perform_model_complexity)
 
+    # Neural Network experiments
     neural_network(x_train, x_test, y_train, y_test,
                    x_pca, x_ica, x_kpca, x_rp,
                    kmeans_clusters, gmm_clusters,
